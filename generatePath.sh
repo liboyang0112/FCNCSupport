b97151552f0ccd7c28737146c299a018e9a10532
boyang <boyang.li@cern.ch>
2021-01-02 17:17:23 +0800

    update

--- generatePath.sh
+++ generatePath.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 echo "\newcommand*{\FCNCFigures}{$FCNC_DIR/FCNCFigures}
-\newcommand*{\FCNCTables}{$FCNC_DIR/FCNCTables}" > FCNCpaths.sty
-
-\cp FCNCpaths.sty talk/.
+\newcommand*{\FCNCTables}{$FCNC_DIR/FCNCTables}
+\newcommand*{\FCNCAnalysis}{$FCNC_DIR/FCNCAnalysis}" > FCNCpaths.sty
